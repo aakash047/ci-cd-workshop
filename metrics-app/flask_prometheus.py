5393716ac00c4e258e4158190c23ae5c6e690dfc
veds-g <guptavedant2312@gmail.com>
2024-02-22 12:27:31 +0530

    added app code
    Signed-off-by: veds-g <guptavedant2312@gmail.com>

--- metrics-app/flask_prometheus.py
+++ metrics-app/flask_prometheus.py
@@ -1,41 +1,48 @@
 from flask import Flask, request
 from prometheus_flask_exporter import PrometheusMetrics
+from prometheus_client import Histogram, Counter
+import time
+import random
 
 app = Flask(__name__)
 metrics = PrometheusMetrics(app)
 metrics.info('app_info', 'Application info', version='1.0.3')
 
-by_path_counter = metrics.counter(
-    'by_path_counter', 'Request count by request paths',
-    labels={'path': lambda: request.path}
-)
-
+latency_histogram = Histogram('request_latency_seconds', 'Request latency in seconds', ['path'], buckets=[0.5, 1, 1.5, 2.0, 100.0])
+by_path_counter = Counter('by_path_counter', 'Request count by request paths', ['path'])
 
 @app.route('/url1')
 @metrics.do_not_track()
-@by_path_counter
-def simple_get():
-    return {
-        'status': '200'
-    }
-
-
-@app.route('/url1')
-@metrics.do_not_track()
-@by_path_counter
 def url1_get():
+    request_number = by_path_counter.labels(path='/url1')._value.get() % 10
+    if request_number == 9:  # 10th request
+        latency = random.uniform(50, 100)  
+    else:
+        latency = random.uniform(0, 2)  
+
+    # time.sleep(latency)
+    latency_histogram.labels(path='/url1').observe(latency)
+    by_path_counter.labels(path='/url1').inc()
+
     return {
         'status': '200'
     }
 
-
-@app.route('/url3')
+@app.route('/url2')
 @metrics.do_not_track()
-@by_path_counter
 def url2_get():
+    request_number = by_path_counter.labels(path='/url2')._value.get() % 10
+    if request_number == 9:  # 10th request
+        latency = random.uniform(50, 100)  
+    else:
+        latency = random.uniform(0, 2)  
+
+    # time.sleep(latency)
+    latency_histogram.labels(path='/url2').observe(latency)
+    by_path_counter.labels(path='/url2').inc()
+
     return {
         'status': '200'
     }
-
 
 app.run(host='0.0.0.0', port=5001)
